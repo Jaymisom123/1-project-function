691bbca328087fbc396f96be2c752eb9be365d6a
Jaymisom123 <jaymerego2015@gmail.com>
2025-02-20 17:03:32 -0300

    ENUM -GAMER.OPTIONS

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const machineScore = document.querySelector("#machine-score")
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+const GAMER_OPTION = {
+
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors',
+
+}
+
 const playHuman = (humanChoice) => {
     playTheGame(humanChoice, playMachine())
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAMER_OPTION.ROCK, GAMER_OPTION.PAPER, GAMER_OPTION.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
     console.log(randomNumber);
 
@@ -22,9 +30,9 @@ const playTheGame = (human, machine) => {
     if (human === machine) {
         result.innerHTML = "Deu empate!!!"
     } else if (
-        (human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'scissors' && machine === 'paper') 
+        (human === GAMER_OPTION.PAPER && machine === GAMER_OPTION.ROCK) ||
+        (human === GAMER_OPTION.ROCK && machine === GAMER_OPTION.SCISSORS) ||
+        (human === GAMER_OPTION.SCISSORS && machine === GAMER_OPTION.PAPER)
     ) {
         humanScoreNumber++
         yourScore.innerHTML = humanScoreNumber
